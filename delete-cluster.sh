69d2287c4d69ba18d487ac867cb2bf744afb519e
Hidetake Iwata <int128@gmail.com>
2017-12-08 09:17:30 -0800

    Fix cleanup script

--- delete-cluster.sh
+++ delete-cluster.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -xe
 
+kubectl --namespace hello delete ingress ingress-hello
+
+gcloud compute addresses delete hello-static-ip --global
+gcloud compute addresses list
+
 gcloud container clusters delete sandbox --zone "us-west1-a"
-gcloud compute addresses delete hello-static-ip
